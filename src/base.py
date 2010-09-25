005c02e171e32db5f1f0ed55c45d5a1756ea960f
Chris Soyars <ctso@ctso.me>
2010-09-25 19:44:29 -0400

    Add default value for template values.

--- src/base.py
+++ src/base.py
@@ -6,7 +6,7 @@ class BasePage(webapp.RequestHandler):
     def __init__(self):
         self.tpl_base = os.path.join(os.path.dirname(__file__), 'templates')
 
-    def render(self, values):
+    def render(self, values={}):
         cls = self.__class__.__name__
         tpl = os.path.join(self.tpl_base, "%s.html" % (cls))
         self.response.out.write(template.render(tpl, values))
