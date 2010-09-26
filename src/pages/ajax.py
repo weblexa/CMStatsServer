5f12a2dd3bb7d4bcb0f381efa20b26d785e85e89
Chris Soyars <ctso@ctso.me>
2010-09-25 20:49:02 -0400

    Update summary numbers on main page via ajax calls.

--- src/pages/ajax.py
+++ src/pages/ajax.py
@@ -1,18 +1,23 @@
 from base import BasePage
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from model import Device
+from model import Device, DeviceAggregate
 
 class AjaxDeviceCount(BasePage):
     def get(self):
         self.response.out.write("%s" % Device.getCount())
 
+class AjaxDevicesCount(BasePage):
+    def get(self):
+        self.response.out.write("%s" % DeviceAggregate.getCount())
+
 class LiveCounter(BasePage):
     def get(self):
-        self.render()
+        self.render({'device_count': Device.getCount()})
 
 application = webapp.WSGIApplication(
         [('/ajax/_Device.getCount', AjaxDeviceCount),
+         ('/ajax/_DeviceAggregate.getCount', AjaxDevicesCount),
          ('/ajax/LiveCounter', LiveCounter)], debug=True)
 
 
