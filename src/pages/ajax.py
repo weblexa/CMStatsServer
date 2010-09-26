388b1800e40a770dcf00341246e2688f25564d20
Chris Soyars <ctso@ctso.me>
2010-09-25 20:26:27 -0400

    Add LiveCounter.

---
+++ src/pages/ajax.py
@@ -0,0 +1,23 @@
+from base import BasePage
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from model import Device
+
+class AjaxDeviceCount(BasePage):
+    def get(self):
+        self.response.out.write("%s" % Device.getCount())
+
+class LiveCounter(BasePage):
+    def get(self):
+        self.render()
+
+application = webapp.WSGIApplication(
+        [('/ajax/_Device.getCount', AjaxDeviceCount),
+         ('/ajax/LiveCounter', LiveCounter)], debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
