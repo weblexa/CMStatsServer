9a327a615063c0a46592ba45a46e5b12cf7466d8
Chris Soyars <ctso@ctso.me>
2011-03-20 21:12:04 -0400

    Remove static content, setup views, setup submit view

--- cmstats/__init__.py
+++ cmstats/__init__.py
@@ -2,12 +2,8 @@ from pyramid.config import Configurator
 from cmstats.resources import Root
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
     config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('cmstats.views.my_view',
-                    context='cmstats:resources.Root',
-                    renderer='cmstats:templates/mytemplate.pt')
-    config.add_static_view('static', 'cmstats:static')
+    config.scan('cmstats.views')
+
     return config.make_wsgi_app()
 
--- cmstats/views.py
+++ cmstats/views.py
@@ -1,2 +1,11 @@
-def my_view(request):
-    return {'project':'CMStats'}
+from cmstats.resources import Root
+from pyramid.view import view_config
+
+
+@view_config(context=Root, name="submit", renderer="string")
+def submit(request):
+    device_id = request.params.get('device_id', None)
+    device_name = request.params.get('device_name', None)
+    device_version = request.params.get('device_version', None)
+    device_country = request.params.get('device_country', None)
+    device_carrier_id = request.params.get('device_carrier_id', None)
