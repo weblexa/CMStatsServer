2d75595c15ef10d204d61b103d22de639693925b
Chris Soyars <ctso@ctso.me>
2011-05-03 23:30:53 -0400

    Use custom number formatter instead of locale.

--- cmstats/utils/template.py
+++ cmstats/utils/template.py
@@ -1,5 +1,13 @@
-import locale
-locale.setlocale(locale.LC_ALL, "")
+
 
 def number(input):
-    return locale.format("%d", int(input), True)
+    return int_comma(input)
+
+def int_comma(x):
+    x = int(x)
+    result = ''
+    while x >= 1000:
+        x, r = divmod(x, 1000)
+        result = ",%03d%s" % (r, result)
+    return "%d%s" % (x, result)
+
